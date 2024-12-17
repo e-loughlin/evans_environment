c5b4e5cabff9a83f140f4f4dd380bbaccc2327e8
Evan Loughlin <evan@neara.com>
2024-12-18 09:40:37 +1100

    2024-12-18_09:40:37

--- .config/wezterm/config/appearance.lua
+++ .config/wezterm/config/appearance.lua
@@ -29,9 +29,9 @@ return {
 	-- tab bar
 	enable_tab_bar = true,
 	hide_tab_bar_if_only_one_tab = false,
+	show_tab_index_in_tab_bar = true,
 	use_fancy_tab_bar = false,
 	tab_max_width = 20,
-	show_tab_index_in_tab_bar = false,
 	switch_to_last_active_tab_when_closing_tab = true,
 	tab_bar_at_bottom = true,
 
