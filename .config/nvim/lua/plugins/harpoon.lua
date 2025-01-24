1d745399ba6866750fff53262b919e87118d455b
Evan Loughlin <evan@neara.com>
2025-01-24 15:56:38 +1100

    2025-01-24_15:56:38

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -9,19 +9,20 @@ return {
     -- basic telescope configuration
     local conf = require("telescope.config").values
     local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
+      local function create_finder()
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+        return require("telescope.finders").new_table {
+          results = file_paths,
+        }
       end
-
-      local finder = require("telescope.finders").new_table {
-        results = file_paths,
-      }
 
       require("telescope.pickers")
         .new({}, {
           prompt_title = "Harpoon",
-          finder = finder,
+          finder = create_finder(),
           previewer = conf.file_previewer {},
           sorter = require("telescope.config").values.generic_sorter {},
           attach_mappings = function(prompt_bufnr, map)
@@ -31,7 +32,7 @@ return {
               local current_picker = state.get_current_picker(prompt_bufnr)
 
               table.remove(harpoon_files.items, selected_entry.index)
-              current_picker:refresh(finder)
+              current_picker:refresh(create_finder())
             end)
             return true
           end,
